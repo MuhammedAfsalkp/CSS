f9e928258d1e112c8cfc73c866a03f93ce3eabdc
Muhammed Afsal kp <mohamedafsalkp321@gmail.com>
2022-12-06 11:40:07 +0530

    final

---
+++ firstproject/shared.js
@@ -0,0 +1,53 @@
+var backdrop =document.querySelector('.backdrop')
+var modal = document.querySelector('.modal')
+var modalClodeButton = document.querySelector('.modal__action--negative')
+var selectPlaneButton = document.querySelectorAll('.plan button') 
+var toggleButton = document.querySelector('.toggle-button')
+var mobileNav = document.querySelector('.mobile-nav')
+
+for(var i=0; i< selectPlaneButton.length; i++){
+    selectPlaneButton[i].addEventListener('click',function(){
+        // modal.style.display = 'block'//inline
+        // backdrop.style.display = 'block'
+        // modal.className='open'//overwrite the class
+        //external
+        modal.classList.add('open')
+        backdrop.classList.add('open')
+
+    })
+}
+
+
+backdrop.addEventListener('click',function(){
+    // mobileNav.style.display = 'none'
+    mobileNav.classList.remove('open')
+   closeModal()
+})
+
+if(modalClodeButton){
+    modalClodeButton.addEventListener('click',closeModal)
+
+
+}
+
+function closeModal(){
+    // modal.style.display = 'none'
+    // backdrop.style.display = 'none'
+ 
+    if(modal){
+        modal.classList.remove('open')
+    }
+    backdrop.classList.remove('open')
+   
+
+}
+
+
+toggleButton.addEventListener('click',function(){
+    // mobileNav.style.display = 'block'
+    // backdrop.style.display = 'block'
+    mobileNav.classList.add('open')
+    backdrop.classList.add('open')
+
+})
+''''{:''"}
